9620cd06594eecf8a08e75cc252ca65d62205699
tdakkota <tanc13@yandex.ru>
2022-01-24 16:41:36 +0300

    test: add next benchmark

--- dec_read_test.go
+++ dec_read_test.go
@@ -19,3 +19,32 @@ func TestDecoder_consume(t *testing.T) {
 	d := Decode(r, 1)
 	require.ErrorIs(t, d.consume('"'), r.Err())
 }
+
+func BenchmarkDecoder_next(b *testing.B) {
+	bench := func(input []byte) func(b *testing.B) {
+		return func(b *testing.B) {
+			d := DecodeBytes(input)
+
+			b.ReportAllocs()
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				d.ResetBytes(input)
+				if c, err := d.next(); err != nil {
+					b.Fatal(c, err)
+				}
+			}
+		}
+	}
+
+	for _, tc := range []struct {
+		name  string
+		input string
+	}{
+		{"NoSpace", `{"x": 0}`},
+		{"Space", ` {"x": 0}`},
+		{"6Newlines", "\n\n\n\n\n\n" + ` {"x": 0}`},
+	} {
+		b.Run(tc.name, bench([]byte(tc.input)))
+	}
+}
